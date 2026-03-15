177d9395dff72a46644c6f1ff50d6557800a30cd
JAAvila <jaavila.dev@outlook.com>
2026-03-15 09:18:28 -0500

    (feat): Extensions have been added for Func<T> and Func<Task<T> types

--- Distributed/JAAvila.FluentOperations/TestExtension.Action.cs
+++ Distributed/JAAvila.FluentOperations/TestExtension.Action.cs
@@ -39,6 +39,42 @@ public static partial class TestExtension
         return new AsyncActionOperationsManager(asyncAction, callerName);
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ActionOperationsManager"/> class,
+    /// allowing fluent exception assertions on a function that returns a value.
+    /// The return value is discarded; only exception behavior is tested.
+    /// </summary>
+    /// <typeparam name="T">The return type of the function (discarded during execution).</typeparam>
+    /// <param name="func">The function to test for exception behavior.</param>
+    /// <param name="callerName">The name of the parameter being tested, used for contextual error reporting.</param>
+    /// <returns>An instance of <see cref="ActionOperationsManager"/> to enable exception assertion chaining.</returns>
+    [Pure]
+    public static ActionOperationsManager Test<T>(
+        this Func<T> func,
+        [CallerArgumentExpression("func")] string callerName = ""
+    )
+    {
+        return new ActionOperationsManager(() => { func(); }, callerName);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AsyncActionOperationsManager"/> class,
+    /// allowing fluent exception assertions on an async function that returns a value.
+    /// The return value is discarded; only exception behavior is tested.
+    /// </summary>
+    /// <typeparam name="T">The return type of the async function (discarded during execution).</typeparam>
+    /// <param name="asyncFunc">The async function to test for exception behavior.</param>
+    /// <param name="callerName">The name of the parameter being tested, used for contextual error reporting.</param>
+    /// <returns>An instance of <see cref="AsyncActionOperationsManager"/> to enable async exception assertion chaining.</returns>
+    [Pure]
+    public static AsyncActionOperationsManager Test<T>(
+        this Func<Task<T>> asyncFunc,
+        [CallerArgumentExpression("asyncFunc")] string callerName = ""
+    )
+    {
+        return new AsyncActionOperationsManager(async () => { await asyncFunc(); }, callerName);
+    }
+
     [Obsolete("This method only serves to prevent this type of action.", true)]
     public static void Test(
         this ITestManager manager,
